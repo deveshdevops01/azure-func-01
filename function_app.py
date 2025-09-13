da961c0d61bd3e407d934d5caf499e3611b59cf0
admin <deveshdevops01@outlook.com>
2025-09-13 20:06:02 +0530

    updated with json dump sql connection of az python function

--- function_app.py
+++ function_app.py
@@ -2,6 +2,7 @@ import azure.functions as func
 import logging
 import pyodbc
 import os
+import json
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
@@ -20,17 +21,15 @@ def DBselectOperation(req: func.HttpRequest) -> func.HttpResponse:
     print(records)
     logging.info(records)
     
-    # return func.HttpResponse(
-    #         'Success',
-    #         str(records),
-    #         status_code=200,
-    #         mimetype="application/json"
-    # )
+    
+
+    records = [tuple(records) for records in records]
+    return_body = json.dumps(obj=records, indent=2)
 
     return func.HttpResponse(
-    str(records),
     status_code=200,
     mimetype="application/json",
+    body = return_body
 )
 
 # $env:PYTHON_PATH="C:\Users\admin\AppData\Local\Programs\Python\Python311\python.exe"
